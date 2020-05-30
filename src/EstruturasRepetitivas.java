8d771de249acbb19c1753e9250619d8ae2f20bc2
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2020-05-29 23:55:32 -0300

    Projeto criado

---
+++ src/EstruturasRepetitivas.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class EstruturasRepetitivas {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        int soma = 0;
+        while ( x != 0 ){
+            soma = soma + x;
+            x = sc.nextInt();
+        }
+
+
+
+
+        sc.close();
+
+
+
+    }
+}
