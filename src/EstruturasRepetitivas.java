e0e0133f20ddc2928026500ef01e9c9592f0d50b
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2020-05-30 00:22:14 -0300

    Exercicio finalizado

--- src/EstruturasRepetitivas.java
+++ src/EstruturasRepetitivas.java
@@ -7,19 +7,13 @@ public class EstruturasRepetitivas {
         Scanner sc = new Scanner(System.in);
 
         int x = sc.nextInt();
-
         int soma = 0;
         while ( x != 0 ){
             soma = soma + x;
             x = sc.nextInt();
         }
-
-
-
+        System.out.println(soma);
 
         sc.close();
-
-
-
     }
 }
